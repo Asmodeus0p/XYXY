eae1eb70361ac8b39c678491dce71b27586c5f10
Asmodeus0p <amodeus_p@163.com>
2018-05-05 11:35:39 +0800

    qwertyqwertfffff

--- app/src/main/java/com/jiyun/asmodeus/xyxy/view/fragment/MingShiFragment.java
+++ app/src/main/java/com/jiyun/asmodeus/xyxy/view/fragment/MingShiFragment.java
@@ -127,12 +127,12 @@ public class MingShiFragment extends BaseFragment implements Teachercontract.Tea
 
 //        //直播课程
 
-//
+
 //        liveCoursesBeen.addAll(homeBean.getData().getLiveCourses());
 //        liveCoursesAdapter= new HomeMasterLiveGridViewAdapter(getContext(),liveCoursesBeen);
 //        home_master_live_gridview.setAdapter(liveCoursesAdapter);
 //        liveCoursesAdapter.notifyDataSetChanged();
-//
+
 //        //推荐作业
 //
 //        homewoksBeen.clear();
