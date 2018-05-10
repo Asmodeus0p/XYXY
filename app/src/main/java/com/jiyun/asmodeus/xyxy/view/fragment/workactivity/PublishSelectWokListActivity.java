6a30b046c1d92f15da9fe41555b6cf3603f227b9
Asmodeus0p <amodeus_p@163.com>
2018-05-10 14:58:57 +0800

    Work1

---
+++ app/src/main/java/com/jiyun/asmodeus/xyxy/view/fragment/workactivity/PublishSelectWokListActivity.java
@@ -0,0 +1,15 @@
+package com.jiyun.asmodeus.xyxy.view.fragment.workactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jiyun.asmodeus.xyxy.R;
+
+public class PublishSelectWokListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_select_wok_list);
+    }
+}
